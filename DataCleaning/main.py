492cc8d62e48be675a72ab2a9bcf8b1f683943a9
dennissavchenko <savchenko.denis007@gmail.com>
2024-10-24 19:37:09 +0200

    Handling missing data

--- DataCleaning/main.py
+++ DataCleaning/main.py
@@ -25,3 +25,20 @@ insert_collection('house_prices_data_raw', df)
 
 # Deleting 'country' column because its every value is 'USA' (not useful)
 df = df.drop(columns=['country'])
+
+# Converting 'date' column into datatime format (correcting datatype)
+df['date'] = pd.to_datetime(df['date'])
+
+# Filling missing values in the 'date' column with the next not null entry (as we see the data are sorted by date)
+df['date'] = df['date'].bfill()
+
+# Deletes entries with missing house prices
+df.dropna(subset=['price'], inplace=True)
+
+# Filling missing values in the numerical columns with the mean
+numeric_cols = df.select_dtypes(include=['float64', 'int64']).columns
+df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+
+# Filling missing values in the categorical columns with 'unknown' (could also use removing)
+categorical_cols = df.select_dtypes(include=['object']).columns
+df[categorical_cols] = df[categorical_cols].fillna('unknown')
